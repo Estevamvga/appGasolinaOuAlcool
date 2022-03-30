183417bdd63bafef9731e6c66b7132a608c99b65
Estevamvga <estevam.pereira@outlook.com>
2022-03-30 01:36:11 -0300

    Commit two

--- app/src/main/java/br/edu/edu/appgasolinaoualcool/MainActivity.java
+++ app/src/main/java/br/edu/edu/appgasolinaoualcool/MainActivity.java
@@ -1,8 +1,11 @@
 package br.edu.edu.appgasolinaoualcool;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,15 +17,17 @@ public class MainActivity extends AppCompatActivity {
     Button btcalcular;
     TextView txtResultado;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         carregarComponentes();
         configurarBotaoResultado();
+        
 
     }
+
+    
 
     protected void carregarComponentes() {
         editGasolina=findViewById(R.id.editGasolina);
@@ -30,15 +35,19 @@ public class MainActivity extends AppCompatActivity {
         btcalcular=findViewById(R.id.btCalcular);
         txtResultado=findViewById(R.id.txtResultado);
 
+
     }
 
     protected void configurarBotaoResultado() {
         btcalcular.setOnClickListener(new View.OnClickListener(){
             @Override
+
             public void onClick(View view) {
                 realizaCalculo();
                 limpaCampos();
+                esconderteclado();
             }
+
         });
     }
 
@@ -61,13 +70,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void exibiResultado(String resultado) {
-        txtResultado.setText(resultado);
-    }
-
-    protected void limpaCampos() {
+     protected void limpaCampos() {
         editGasolina.setText("");
         editAlcool.setText("");
         editGasolina.requestFocus();
+    }
+
+    private void esconderteclado() {
+
+        View view = this.getCurrentFocus();
+        if(view !=null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
     }
 }
